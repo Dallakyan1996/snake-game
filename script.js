96bc3b3ee0ff1ac0657bfd487c7da999aaed5fd4
d_ara <d.ara@clinsoftcsd.com>
2023-01-07 21:25:16 +0400

    added foot eating

--- script.js
+++ script.js
@@ -1,54 +1,119 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const snakeData = [
-    {
-        x: 80,
-        y: 10,
-    },
-    {
-        x: 45,
-        y: 10,
-    },
-    {
-        x: 10,
-        y: 10,
+const cellSize = 20;
+
+const canvasSize = 600;
+canvas.width = canvasSize;
+canvas.height = canvasSize;
+
+const mapCount = canvasSize / cellSize;
+
+let direction = {
+  x: 0,
+  y: 0,
+};
+
+const initialTail = [
+  {
+    x: 4,
+    y: 4,
+  },
+  {
+    x: 4,
+    y: 4,
+  },
+  {
+    x: 4,
+    y: 4,
+  },
+  {
+    x: 4,
+    y: 4,
+  },
+];
+
+let tail = [...initialTail];
+
+let food = { x: randomCoord().x, y: randomCoord().y };
+
+function randomNum() {
+  return Math.floor(Math.random() * mapCount);
+}
+
+console.log(food);
+function randomCoord() {
+  const x = randomNum();
+  const y = randomNum();
+  if (isCoordOnSnake(x, y)) {
+    return randomCoord();
+  } else {
+    return { x, y };
+  }
+}
+
+function isCoordOnSnake(x, y) {
+  let isOnSnake = false;
+  tail.forEach((cell) => {
+    if (cell.x === x && cell.y === y) {
+      isOnSnake = true;
     }
-]
-
-
-function update() {
-    snakeData.forEach(item => {
-        item.x += 1
-    })
-}
-
-function createGameBoard() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, 500, 800)
-}
-
-function createSnakeBlock() {
-    snakeData.forEach(item => {
-        ctx.fillStyle = "red",
-            ctx.fillRect(item.x, item.y, 30, 30)
-    })
+  });
+  return isOnSnake;
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    createGameBoard();
-    snakeData.forEach(item => {
-        ctx.fillStyle = "red",
-            ctx.fillRect(item.x, item.y, 30, 30)
-    })
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  tail.forEach((cell) => {
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+    ctx.strokeRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize);
+  });
+
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(food.x * cellSize, food.y * cellSize, cellSize, cellSize);
 }
 
-function loop() {
-    requestAnimationFrame(loop);
-  
-    update();
+setInterval(() => {
+  if (direction.x === 0 && direction.y === 0) {
     draw();
-}
+    return;
+  }
+  const newCell = {
+    x: (mapCount + tail[tail.length - 1].x + direction.x) % mapCount,
+    y: (mapCount + tail[tail.length - 1].y + direction.y) % mapCount,
+  };
 
-loop();
+  if (newCell.x === food.x && newCell.y === food.y) {
+    tail.push(newCell);
+    food = { x: randomCoord().x, y: randomCoord().y };
+  } else {
+    tail.shift();
+    tail.push(newCell);
+  }
+  draw();
+}, 1000 / 40);
 
+document.addEventListener("keydown", function (evt) {
+  switch (evt.code) {
+    case "ArrowUp":
+      direction.x = 0;
+      direction.y = -1;
+      break;
+    case "ArrowDown":
+      direction.x = 0;
+      direction.y = 1;
+      break;
+
+    case "ArrowLeft":
+      direction.x = -1;
+      direction.y = 0;
+      break;
+    case "ArrowRight":
+      direction.x = 1;
+      direction.y = 0;
+      break;
+  }
+});
